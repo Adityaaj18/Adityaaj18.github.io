672bda9b47138b413469569611853799a6b6bfeb
Aditya Jadhav <39948420+Adityaaj18@users.noreply.github.com>
2021-10-05 13:43:59 +0530

    finished

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,49 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import React from 'react'
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai'
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import { SocialIcons } from '../Header/HeaderStyles'
+import {
+   CompanyContainer,
+   FooterWrapper,
+   LinkColumn,
+   LinkItem,
+   LinkList,
+   LinkTitle,
+   Slogan,
+   SocialContainer,
+   SocialIconsContainer
+} from './FooterStyles'
 
 const Footer = () => {
-  return (
-    <div>
-      Footer
-    </div>
-  );
-};
+   return (
+      <FooterWrapper>
+         <LinkList>
+            <LinkColumn>
+               <LinkTitle>Call</LinkTitle>
+               <LinkItem href="tel:11111-11111">11111-11111</LinkItem>
+            </LinkColumn>
+            <LinkColumn>
+               <LinkTitle>Email</LinkTitle>
+               <LinkItem href="mailto:jadhavaditya033@gmail.com">
+                  jadhavaditya033@gmail.com
+               </LinkItem>
+            </LinkColumn>
+         </LinkList>
+         <SocialIconsContainer>
+            <SocialContainer>
+               <SocialIcons href="https://github.com">
+                  <AiFillGithub size="3rem" />
+               </SocialIcons>
+               <SocialIcons href="https://linkedin.com">
+                  <AiFillLinkedin size="3rem" />
+               </SocialIcons>
+               <SocialIcons href="https://instagram.com">
+                  <AiFillInstagram size="3rem" />
+               </SocialIcons>
+            </SocialContainer>
+         </SocialIconsContainer>
+      </FooterWrapper>
+   )
+}
 
-export default Footer;
+export default Footer
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,59 +9,49 @@ import {
    Div2,
    Div3,
    NavLink,
-   SocialIcons,
-   Span
+   SocialIcons
 } from './HeaderStyles'
 
 const Header = () => (
-   <div>
-      <Container>
-         l
-         <Div1>
-            <Link href="/">
-               <a
-                  style={{
-                     display: 'flex',
-                     alignments: 'center',
-                     color: 'white',
-                     marginBottom: '20px'
-                  }}
-               >
-                  <DiCssdeck size="3rem" />
-                  <Span>Portfolio</Span>
-               </a>
+   <Container>
+      <Div1>
+         <Link href="/">
+            <a
+               style={{ display: 'flex', alignItems: 'center', color: 'white' }}
+            >
+               <DiCssdeck size="3rem" /> <span>Portfolio</span>
+            </a>
+         </Link>
+      </Div1>
+      <Div2>
+         <li>
+            <Link href="#projects">
+               <NavLink>Projects</NavLink>
             </Link>
-         </Div1>
-         <Div2>
-            <li>
-               <Link href="#projects">
-                  <NavLink>Projects</NavLink>
-               </Link>
-            </li>
-            <li>
-               <Link href="#tech">
-                  <NavLink>Technologies</NavLink>
-               </Link>
-            </li>
-            <li>
-               <Link href="#about">
-                  <NavLink>About</NavLink>
-               </Link>
-            </li>
-         </Div2>
-         <Div3>
-            <SocialIcons href="https://github.com">
-               <AiFillGithub size="3rem" />
-            </SocialIcons>
-            <SocialIcons href="https://linkedin.com">
-               <AiFillLinkedin size="3rem" />
-            </SocialIcons>
-            <SocialIcons href="https://instagram.com">
-               <AiFillInstagram size="3rem" />
-            </SocialIcons>
-         </Div3>
-      </Container>
-   </div>
+         </li>
+         <li>
+            <Link href="#tech">
+               <NavLink>Technologies</NavLink>
+            </Link>
+         </li>
+         <li>
+            <Link href="#about">
+               <NavLink>About</NavLink>
+            </Link>
+         </li>
+      </Div2>
+      <Div3>
+         <SocialIcons href="https://google.com">
+            <AiFillGithub size="3rem" />
+         </SocialIcons>
+         <SocialIcons href="https://google.com">
+            <AiFillLinkedin size="3rem" />
+         </SocialIcons>
+         <SocialIcons href="https://google.com">
+            <AiFillInstagram size="3rem" />
+         </SocialIcons>
+      </Div3>
+   </Container>
 )
 
 export default Header
